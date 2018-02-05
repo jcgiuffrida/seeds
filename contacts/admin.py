06eefff2ced243183a2eefc3eb27deab5e7ab8f2
Jonathan Giuffrida <jcgiuffri@gmail.com>
2018-02-04 19:22:35 -0600

    Better models

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,18 @@
 """Admin for the main models."""
 from django.contrib import admin
 
-from .models import Contact
+from .mixins import AuditingAdminModelMixin
+from .models import Person, Company, Group
 
-admin.site.register(Contact)
+class PersonAdmin(AuditingAdminModelMixin, admin.ModelAdmin):
+    list_filter = ['date_added']
+
+class CompanyAdmin(AuditingAdminModelMixin, admin.ModelAdmin):
+    list_filter = ['date_added']
+
+class GroupAdmin(AuditingAdminModelMixin, admin.ModelAdmin):
+    list_filter = ['date_added']
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Group, GroupAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -2,13 +2,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Contact(models.Model):
+from .mixins import AuditingModel
+
+class Person(AuditingModel):
     first_name = models.CharField(max_length=64, default='', blank=True)
     last_name = models.CharField(max_length=64, default='', blank=True)
     email = models.EmailField(default='', blank=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, editable=False, related_name='contacts')
-    timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+    
+    class Meta:
+        verbose_name_plural = 'people'
 
     @property
     def full_name(self):
-        return (self.first_name + ' ' + self.last_name).trim()
+        return (self.first_name + ' ' + self.last_name).strip()
+
+    def __str__(self):
+        return self.full_name
+
+class Company(AuditingModel):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'companies'
+
+    def __str__(self):
+        return self.name
+    
+
+class Group(AuditingModel):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=64, unique=True, blank=True)
+    about = models.TextField(blank=True)
+    people = models.ManyToManyField(Person, blank=True)
+    companies = models.ManyToManyField(Company, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
